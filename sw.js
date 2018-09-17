980932980bb05ba453ff718a95c4a4dcdae2fa7e
trong-nguyen <nguyentrong@gmail.com>
2018-09-17 00:36:06 -0400

    Service workers works! Need a little more polish and we're done

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
-var appCache = 'restaurant-app-v1';
+var appCache = 'restaurant-app-v2';
 var dynamicCache = 'restaurant-content';
 var allCaches = [
     appCache,
     dynamicCache
+];
+
+var appCacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/js/dbhelper.js',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
 ];
 
 self.addEventListener('install', event => {
@@ -10,18 +23,17 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(appCache).then(cache => {
             console.log('caching', caches);
-            self.p = cache.addAll([
-                '/index.html',
-                '/restaurant.html',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/js/dbhelper.js',
-                '/css/styles.css'
-            ]);
-            return p;
+            return cache.addAll(appCacheFiles);
         })
     );
 });
+
+function fetchNew(request, cache) {
+    return fetch(request).then(response => {
+        cache.put(request.url, response.clone());
+        return response;
+    });
+}
 
 self.addEventListener('activate', function(event) {
     event.waitUntil(
@@ -39,14 +51,38 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.open(dynamicCache).then(cache => {
-            return fetch(event.request).then(response => {
-                cache.put(event.request.url, response.clone());
-                return response;
-            });
-        })
-    );
+    var url = new URL(event.request.url);
+    self.arr = self.arr || [];
+    self.arr.push(event.request);
+
+    if (url.pathname !== '/restaurant.html' &&
+        (appCacheFiles.includes(url.pathname)
+        // this one for leafletjs
+        || appCacheFiles.includes(event.request.url))) {
+
+        console.log('serving skeleton files', event.request);
+        event.respondWith(caches.match(url));
+    }
+
+    // only cache the data from our site
+    else if (url.origin === location.origin) {
+        event.respondWith(
+            caches.open(dynamicCache).then(cache => {
+                return cache.match(url).then(response => {
+                    // In this implementation, clients effectively
+                    // lag behind in terms of when data get updated by 1 request.
+                    if (response) {
+                        fetchNew(event.request, cache);
+                        return response;
+                    }
+
+                    return fetchNew(event.request, cache);
+                })
+            })
+        );
+    } else {
+        event.respondWith(fetch(event.request));
+    }
 });
 
 self.addEventListener('message', event => {
